d442434474abd0722ced1a2407c85ff86a169122
JanSkla <jan.skladal11@gmail.com>
2024-06-22 17:49:29 +0200

    type added to create/join message

--- src/ws/wsService.js
+++ src/ws/wsService.js
@@ -10,7 +10,7 @@ export const createRoomService = ws => {
 
   const status = joinData == false ? 400 : 200; 
 
-  ws.send(JSON.stringify({status: status, code: roomCode, pId: joinData.player.pId}));
+  ws.send(JSON.stringify({type: "create-room" ,status: status, code: roomCode, pId: joinData.player.pId}));
 
   return joinData;
 }
@@ -29,7 +29,7 @@ export const joinRoomService = (ws, code) => {
   const joinData = joinPlayerToRoom(room, { name: "pepik", ws: ws });
 
 
-  const response = !!joinData ? {status: 200, pId: joinData.player.pId} : {status: 400};
+  const response = !!joinData ? {type: "join-room" ,status: 200, pId: joinData.player.pId} : {status: 400};
 
   ws.send(JSON.stringify(response));
 
